4fcc04c3c0cfbb5f5d32b727bd2bf26da76a5d2d
ashclayj <ashclayj@iu.edu>
2025-03-19 19:30:50 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Tasks from './Tasks'; // Import Tasks component
+
+function App() {
+    return (
+        <div className="App">
+            <h1>SmartTask App</h1>
+            <Tasks />  {/* Show task list */}
+        </div>
+    );
+}
+
+export default App;
+
